51f03aa9a9dab54b23c22da799b3be6bb0e3968e
benson momodu <benson.isaac.momodu@gmail.com>
2019-06-27 09:15:45 +0100

    styling the loan interface

--- src/User/loan.js
+++ src/User/loan.js
@@ -8,17 +8,49 @@ console.log(loans)
 
 loans.map( loan => {
     console.log(loan.name)
-    displayLoan += `
+    displayLoan.innerHTML += `
         <div>
-            <ul>
-                <li>
-                    <span>Name${loan.name}</span>
-                    <span>LoanAtm${loan.loanAmt}</span>
-                    <span><button>Aprove</button></span>
-                    <span><button>Decline</button></span>
+            <ul class="list-group">
+                <li id="list">
+                    <span>Name: ${loan.name}</span>
+                    <span>LoanAtm: ${loan.loanAmt}#</span>
+                    <span class="btn"><button class="pending" onclick="pendingHandlerFunc()" id="pending">pending</button></span>
+                    <div id="action-btn" class="action-btn">
+                        <button onclick="declineHandlerFunc()"    class="decline">decline</button>
+                        <button onclick="approveHandlerFunc()" class="approve">approve</button>
+                    </div>
                 </li>
             </ul>
         </div>
     
     `
 })
+
+let actionBtns =  document.getElementById("action-btn")
+let pendingBtn = document.getElementById("pending")
+
+//Event
+
+//handle pending button
+function pendingHandlerFunc() {  
+    actionBtns.style.display = "block"
+}
+
+//handle approve button
+function approveHandlerFunc() {  
+    actionBtns.style.display = "none"
+    pendingBtn.innerText = "approve"
+    pendingBtn.style.backgroundColor = 'green'
+    pendingBtn.style.color = 'white'
+
+}
+
+//handle decline button
+function declineHandlerFunc() {  
+    actionBtns.style.display = "none"
+    pendingBtn.innerText = "decline"
+    pendingBtn.style.backgroundColor = 'darkorange'
+    pendingBtn.style.color = 'white'
+
+}
+
